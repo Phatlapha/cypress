27b27251dbb1b7496ae9086cf0cd0ba3b89aaec3
Phatlapha <Phatlapha.tp@gmail.com>
2023-09-24 00:33:12 +0700

    Initializing repo

---
+++ cypress/e2e/swag.spec.js
@@ -0,0 +1,9 @@
+describe("Log in page component", () => {});
+
+describe("Inventory page component", () => {});
+
+describe("Sorting filter component", () => {});
+
+describe("Address component", () => {});
+
+describe("E2E", () => {});
