8ba9e34ba6d275b1cbd80595a3ab9dec989857f7
Phatlapha <Phatlapha.tp@gmail.com>
2023-09-27 23:45:19 +0700

    add tests for inventory page

--- cypress/e2e/swag.spec.js
+++ cypress/e2e/swag.spec.js
@@ -61,8 +61,48 @@ describe("Log in page component", () => {
     });
 });
 
-describe("Inventory page component", () => {});
+describe("Inventory page component", () => {
+    beforeEach(() => {
+        cy.visit("https://www.saucedemo.com/v1/index.html");
+        const login = new loginPage();
+        login.loginSuccess();
+    });
 
+    it("Should add product to cart", () => {
+        cy.get(".inventory_item").first();
+        cy.get(".btn_primary.btn_inventory").first().click();
+        cy.get(".fa-layers-counter.shopping_cart_badge").should("be.visible");
+    });
+
+    it("should add more product to cart", () => {
+        cy.get(".inventory_item").first();
+        cy.get(".btn_primary.btn_inventory").first().click();
+        cy.get(".btn_primary.btn_inventory").last().click();
+        cy.get(".fa-layers-counter.shopping_cart_badge").should("be.visible");
+    });
+
+    it("should remove product to cart", () => {
+        cy.get(".inventory_item").first();
+        cy.get(".btn_primary.btn_inventory").first().click();
+        cy.get(".btn_secondary.btn_inventory").first().click();
+        cy.get(".fa-layers-counter.shopping_cart_badge").should("not.exist");
+    });
+
+    it("should change page when click at a product pic", () => {
+        cy.get(".inventory_item").first().find("div.inventory_item_img").click();
+        cy.url().should("include",'inventory-item.html?id=4')
+    });
+
+    it("should change when click filters", () => {
+        cy.get(".inventory_item").first().find(".inventory_item_name").should("have.text",'Sauce Labs Backpack');
+        cy.get(".product_sort_container").select("Name (Z to A)").should("have.value", "za");
+        cy.get(".inventory_item").first().find(".inventory_item_name").should("have.text","Test.allTheThings() T-Shirt (Red)");
+        cy.get(".product_sort_container").select("Price (low to high)").should("have.value", "lohi");
+        cy.get(".inventory_item").first().find(".inventory_item_name").should("have.text","Sauce Labs Onesie");
+        cy.get(".product_sort_container").select("Price (high to low)").should("have.value", "hilo");
+        cy.get(".inventory_item").first().find(".inventory_item_name").should("have.text","Sauce Labs Fleece Jacket");
+    });
+});
 describe("Sorting filter component", () => {});
 
 describe("Address component", () => {});
